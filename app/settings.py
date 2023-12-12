35a33450e0bb4c33d5180e79709df0163ff772ba
Niolum <al_belov98@mail.ru>
2023-12-12 20:21:48 +0700

    add python-dotenv

--- app/settings.py
+++ app/settings.py
@@ -3,11 +3,16 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 
 class Setting:
     """This configuration used to get the database
       connection string from the environment variables.
     """
+    BASE_DIR = Path(__file__).parent.parent.resolve()
+    load_dotenv(os.path.join(BASE_DIR, ".env"))
+
     DB_USER: str | None = os.getenv("DB_USER")
     DB_PASSWORD: str | None=os.getenv("DB_PASSWORD")
     DB_HOST: str | None=os.getenv("DB_HOST")
@@ -16,7 +21,6 @@ class Setting:
         "DB_CONFIG",
         f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}",
     )
-    BASE_URL = Path(__file__).parent.parent.resolve()
 
 
 setting = Setting()
